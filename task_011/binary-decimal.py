2be6da11680593a2b2db95f795733a7cf3650a4f
Sayan Banerjee <119781497+sayan14banerjee@users.noreply.github.com>
2023-09-16 18:22:13 +0530

    update task 11

--- task_011/binary-decimal.py
+++ task_011/binary-decimal.py
@@ -1,14 +1,15 @@
 class BinaryToDecimal:
     def convert(self, num):
-        sum = 0
         pow = int(len(num) - 1)
+        sum = 0
         for i in num:
-            sum += int(i) * 2 ** pow  # it calculate the decimal
+            sum += int(i) * 2 ** pow # it calculate the decimal
+
+            pow -=1
         return sum
 
-
 class Operation:
-    def __int__(self, num1, num2):
+    def __init__(self, num1, num2):
         self.num1 = num1
         self.num2 = num2
 
@@ -25,13 +26,13 @@ class Operation:
         return self.num1 / self.num2
 
     def square(self):
-        return self.num1 ** 2 and self.num2 ** 2
+        print(f"num 1 square { self.num1 ** 2} and num2 square {self.num2 ** 2}")
 
 
 if __name__ == "__main__":
     try:
-        num1 = (input("Enter number in binary"))
-        num2 = (input("Enter number in binary"))
+        num1 = (input("Enter number in binary "))
+        num2 = (input("Enter number in binary "))
         for i in num1 :
             if i not in {'0', '1'}:  # check the input is binary or not
                 raise ValueError()
@@ -44,13 +45,15 @@ if __name__ == "__main__":
             else:
                 ans = BinaryToDecimal()
                 n2 = ans.convert(num2)
+        
+        print(n1 ,'\n', n2)
+        op = Operation(n1, n2)
+        print(f"sum = {op.sum()}")
+        print(f"sub = {op.sub()}")
+        print(f"mul = {op.mul()}")
+        print(f"div = {op.div()}")
+        op.square()
     except ValueError:
         print('Enter only 1 or 0')
 
-    print(n1, '\n', n2)
-    # op = Operation(n1, n2)
-    # print(f"sum = {op.sum()}")
-    # print(f"sub = {op.sub()}")
-    # print(f"mul = {op.mul()}")
-    # print(f"div = {op.div()}")
-    # print(f"square = {op.square()}")
+   
